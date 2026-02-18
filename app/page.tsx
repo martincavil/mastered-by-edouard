8875b630258dcf7168a8986349e60d98dcc70430
martincavil <martincavil.dev@gmail.com>
2026-02-18 14:25:36 +0100

    fix font sizzing homepage mobile

--- app/page.tsx
+++ app/page.tsx
@@ -61,10 +61,10 @@ export default function HomePage() {
           />
           <div className="space-y-10 md:space-y-13 relative z-10 w-full">
             <div className="px-4 md:px-8">
-              <h1 className="text-5xl md:text-7xl font-medium tracking-tight text-white text-shadow-lg">
+              <h1 className="text-4xl md:text-7xl font-medium tracking-tight text-white text-shadow-lg">
                 {t.home.title}
               </h1>
-              <h2 className="text-[30px]/9 md:text-7xl/11 lg:text-6xl/9 text-white text-shadow-lg font-extralight">
+              <h2 className="text-2xl/9 md:text-7xl/11 lg:text-6xl/9 text-white text-shadow-lg font-extralight">
                 {t.home.subtitle}
               </h2>
             </div>
